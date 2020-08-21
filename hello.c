5073d59ddac7473b5b4b2d8893af2b7dbf644863
StupidWill <wcook2@huskers.unl.edu>
2020-08-21 15:56:50 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Will Cook
+ *         wcook2@huskers.unl.edu
+ * Date: 2020/08/19
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Will Cook\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
